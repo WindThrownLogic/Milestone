c717fe052be1dc307cffb67e6f5f840e9e83c6c6
CarltonM <carltonmugo@gmail.com>
2017-10-26 11:18:35 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,8 @@
 public static void main(String[] args) {
+    
+    int two = 3;
+    String name = "notname";
+    
     int[] series = {4,2};
     series = addElement(series, 3);
     series = addElement(series, 1);
